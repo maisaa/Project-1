1275c429d0de40b18f2c496833c55fdbd152e68c
maisaa <maisaa.alkhedr@gmail.com>
2021-04-24 18:02:49 +0200

    add images array to main.js

---
+++ main.js
@@ -0,0 +1,82 @@
+// window.addEventListener('DOMContentLoaded', (event) => {
+//     console.log('DOM fully loaded and parsed');
+// });
+
+document.addEventListener('DOMContentloaded', () => {
+    const cardArr = [
+        {
+            image: 'abu',
+            src: 'images/abu.jpj'
+        },
+        {
+            image: 'aladdin',
+            src: 'images/aladdin.png'
+        },
+        {
+            image: 'Carpet',
+            src: 'images/Carpet.jpg'
+        },
+        {
+            image: 'genie1',
+            src: 'images/genie1.jpg'
+        },
+        {
+            image: 'j4',
+            src: 'images/j4.jpg'
+        },
+        {
+            image: 'sultan',
+            src: 'images/sultan.jpg'
+        },
+        {
+            image: 'retu',
+            src: 'images/retu.jpg'
+        },
+        {
+            image: 'jafar',
+            src: 'images/jafar.jpg'
+        },
+        {
+            image: 'abu',
+            src: 'images/abu.jpj'
+        },
+        {
+            image: 'aladdin',
+            src: 'images/aladdin.png'
+        },
+        {
+            image: 'Carpet',
+            src: 'images/Carpet.jpg'
+        },
+        {
+            image: 'genie1',
+            src: 'images/genie1.jpg'
+        },
+        {
+            image: 'j4',
+            src: 'images/j4.jpg'
+        },
+        {
+            image: 'sultan',
+            src: 'images/sultan.jpg'
+        },
+        {
+            image: 'retu',
+            src: 'images/retu.jpg'
+        },
+        {
+            image: 'jafar',
+            src: 'images/jafar.jpg'
+        },
+    ]
+
+// add here new variables and functions
+
+// add two arrays  one for checked Cards and the second one for won Cards 
+
+//
+
+})
+
+// console.log("hi main.js")
+
